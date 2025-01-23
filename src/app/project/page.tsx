900a6056bb9e86e2486ae2a6d0201dc837713bd7
Lee-Pui-Shan <puishan.lee2000@gmail.com>
2025-01-23 14:22:37 +0800

    added suspense

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,11 +1,14 @@
 import { getComment } from "@/actions/comment_action";
 import ProjectTabs from "@/components/project/projectTabs";
+import { Suspense } from "react";
 
 
 export default async function Project() {
     return (
+        <Suspense>
         <div className="flex justify-center justify-items-center">
             <ProjectTabs />
         </div>
+        </Suspense>
     )
 }
