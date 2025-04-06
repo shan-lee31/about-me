64d1e128b4693b6dfd35ef7d3ed52f02619eae08
Lee-Pui-Shan <puishan.lee2000@gmail.com>
2025-04-06 23:45:48 +0800

    Update on the projects

--- src/components/project/projectTabs.tsx
+++ src/components/project/projectTabs.tsx
@@ -1,11 +1,13 @@
+"use client"
 import { Label } from "../ui/label";
+import ProjectCards from "./projectCards";
 
 const ProjectTabs = () => {
     return (
         <>
             <Label className="font-bold text-2xl">Projects</Label>
-            <div className="mt-10 border h-96 w-full">
-               
+            <div className="mt-10 border h-auto w-full">
+                <ProjectCards />
             </div>
         </>
     )
