f355fc37a28bab9fc7a6b6ee1aad1944ecce67e5
Lee-Pui-Shan <puishan.lee2000@gmail.com>
2025-05-15 09:12:23 +0800

    Update email

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -4,16 +4,15 @@ import Link from "next/link";
 
 const ContactMe = () => {
     const socialAcc = {
-        "Email": [<Mail />, "puishan.lee2000@gmail.com", "puishan.lee2000@gmail.com"],
+        "Email": [<Mail />, "puishan.lee2000@gmail.com", "mailto:puishan.lee2000@gmail.com"],
         "Github": [<Github />, "shan-lee31", "https://github.com/shan-lee31/"],
         "LinkedIn": [<Linkedin />, "Lee Pui Shan", "https://www.linkedin.com/in/puishan-lee-86099b196"]
     }
-    const socialLinks = ["a", "b", "c"]
+    
     return (
         <>
             <Label className="font-bold text-2xl">Contact Me</Label>
             <div className="mt-10 w-full">
-                {/* {Object.entries(socialLinks).map(([platform, contacts]) => ( */}
                 <div className={`grid md:grid-cols-3 grid-rows-3 md:grid-rows-1 mb-10`}>
                     {Object.entries(socialAcc).map(([platform, contacts]) => (
                         <div className="md:col-span-1 row-span-1 mb-10" key={platform}>
